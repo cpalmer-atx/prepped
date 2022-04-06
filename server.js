9b02ee04e832ad5842744089f544e693c2f5721e
Chad Palmer <chad.n.palmer@gmail.com>
2022-04-06 15:54:19 -0500

    connected user route to server, GET request test successful

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express = require('express');
 
 const app = express();
 
+const users = require('./routes/users');
+
 app.use(express.json());
+
+app.use('/api/users', users);
 
 const PORT = process.env.PORT || 5500;
 
