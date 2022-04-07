d9346f4cfcbc2703ec58488ddb2dd3ea105bd8f4
Chad Palmer <chad.n.palmer@gmail.com>
2022-04-07 13:07:09 -0500

    server now persists new user to database

--- controllers/users.js
+++ controllers/users.js
@@ -7,25 +7,18 @@ exports.registerUser = async (req, res) => {
     const { name, email, password } = req.body;
 
     try {
-        /* Uncomment when database is connected */
-        // let user = await User.findOne({ email });
-        // if (user) {
-        //     return res.status(400).json({ msg: 'user already exists' });
-        // }
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ msg: 'user already exists' });
+        }
 
-        user = new User({
-            name,
-            email,
-            password
-        });
+        user = new User({ name, email, password });
+        const payload = { user: { id: user.id } };
 
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(password, salt);
-        /* Uncomment when database is connected */
-        // await user.save();
-        console.log(user);
+        await user.save();
 
-        const payload = { user: { id: user.id } };
         jwt.sign( 
             payload, 
             process.env.JWT_SECRET,
