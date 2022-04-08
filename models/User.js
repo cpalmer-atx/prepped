124a9064c4e7aa6abc860158054640000808a33c
Chad Palmer <chad.n.palmer@gmail.com>
2022-04-08 15:32:19 -0500

    added cards to user model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,19 @@
 const mongoose = require('mongoose');
 
+const CardSchema = new mongoose.Schema({
+    question: {
+        type: String,
+        required: true
+    },
+    answer: {
+        type: String,
+        required: true
+    },
+    proficient: {
+        default: false
+    }
+});
+
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -16,7 +30,8 @@ const UserSchema = new mongoose.Schema({
     date: {
         type: Date,
         default: Date.now
-    }
+    },
+    cards: [ CardSchema ]
 });
 
 const User = module.exports = mongoose.model('User', UserSchema);
