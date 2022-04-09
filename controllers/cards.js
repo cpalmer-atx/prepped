eefb95aa25eebb401b73577dfcf6d611f0e566d0
Chad Palmer <chad.n.palmer@gmail.com>
2022-04-09 13:02:06 -0500

    implemented deleteCard, deletes a card by id

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,3 +1,4 @@
+const { db } = require('../models/User');
 const User = require('../models/User');
 
 exports.addCard = async (req, res) => {
@@ -28,6 +29,31 @@ exports.addCard = async (req, res) => {
     }
 }
 
-// exports.deleteCard = async (req, res) => {
-//     return {};
-// }
+
+exports.deleteCard = async (req, res) => {
+    const { id } = req.body;
+    
+    try {
+        // Find user in database
+        const user = await User.findById(req.user.id).select('-password');
+        if (!user) {
+            return res.status(400).json({ msg: 'No user found' });
+        }
+
+        // Filter out all cards to keep into a newCardList
+        const newCardList = user.cards.filter( card => { return card.id !== id });
+        if (newCardList.length === user.cards.length) {
+            return res.status(400).json({ msg: 'No card found' });
+        }
+
+        // Replace user's cards with the newCardList and save the user collection
+        user.cards = newCardList;
+        user.save();
+
+        // Return the user's updated cards list
+        return res.status(200).send(user.cards);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send(err.message);
+    }
+}
--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth')
-const { addCard } = require('../controllers/cards');
+const { addCard, deleteCard } = require('../controllers/cards');
 
 router
     .route('/')
-    .post(auth, addCard);
+    .post(auth, addCard)
+    .delete(auth, deleteCard);
 
 module.exports = router;
