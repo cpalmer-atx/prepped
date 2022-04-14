18e897ffd5ad0756225ba488008a3edcd7c08f2a
Chad Palmer <chad.n.palmer@gmail.com>
2022-04-14 16:17:41 -0500

    form outline is there, need to fix autofill and add margins to textfields

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,22 +1,85 @@
 import styled from '@emotion/styled';
-import { Paper } from '@mui/material';
+import { Paper, Typography, TextField, Button, InputAdornment, IconButton } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material/';
 import '../App.css';
+import { useState } from 'react';
 
 
 const Form = styled(Paper)( () => ({
     backgroundColor: '#fff',
     padding: 10,
+    // width: 400,
     textAlign: 'center'
 }));
 
+const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('Login request submitted!');
+}
+
+const handleMouseDown = (e) => {
+    e.preventDefault();
+}
+
 const LoginForm = () => {
+    const [ showPwd, setShowPwd ] = useState(false);
+
   return (
       
-    <Form 
-    xs={10} 
-    elevation={24}
-    >
-        <h1>Login Form</h1>
+    <Form xs={10} elevation={24}>
+        <Typography
+            variant="h5"
+            
+            color="textSecondary"
+            gutterBottom
+        >   Sign In
+        </Typography>
+        <form onSubmit={handleSubmit} autoComplete='false'>
+            <TextField
+                name='username'
+                label='Name'
+                placeholder="username"
+                variant="outlined"
+                type='text'
+                // value={name}
+                // onChange={handleSetName}
+                // error={nameError}
+                fullWidth
+                required      
+            />
+            <TextField
+                name='password'
+                label='Password'
+                placeholder="password"
+                type={showPwd ? 'text' : 'password'}
+                variant="outlined"
+                fullWidth
+                required
+                // value={name}
+                // onChange={handleSetName}
+                // error={nameError}
+                InputProps={{
+                    endAdornment:(
+                        <InputAdornment position="end">
+                        <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={ () => setShowPwd(!showPwd) }
+                        onMouseDown={handleMouseDown}
+                        edge="end"
+                        >   {showPwd ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                        </InputAdornment>
+                    )
+                }}
+            />
+            <Button
+                type="submit"
+                color='primary'
+                variant='contained'
+                fullWidth
+            >   Login
+            </Button>
+        </form>
     </Form> 
   );
 }
