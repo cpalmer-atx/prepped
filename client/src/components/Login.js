0848825e8c8f4df3edfa7c67be2e5262baa048b1
Chad Palmer <chad.n.palmer@gmail.com>
2022-04-14 15:07:50 -0500

    login page looking better than register

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,33 @@
+import styled from '@emotion/styled';
+import { Paper, Grid } from '@mui/material';
+import { Box, padding } from '@mui/system';
 import '../App.css';
+
+
+const Item = styled(Paper)( () => ({
+    backgroundColor: '#fff',
+    padding: 10,
+    textAlign: 'center'
+}));
 
 const Login = () => {
   return (
-    
-    <div className="login">
-      <h1>Reserved for Login</h1>
-    </div>
+      <Grid
+        container
+        spacing={0}
+        padding={5}
+        direction='column'
+        alignItems='center'
+        justifyContent="center"
+        style={{ minHeight: '90vh'}}
+      >
+          <Item 
+            xs={10} 
+            elevation={24}
+          >
+              <h1>Reserved for Login Form what happens if this is long....</h1>
+          </Item> 
+      </Grid>
   );
 }
 
